69129e797229ee80d0b7c93690a15e351cf8613b
WSCU <lucas.vandenbergh@western.edu>
2014-01-23 11:58:32 -0700

    Delete reactiveengine.py

--- src/reactiveengine.py
+++
@@ -1,9 +0,0 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-__author__="jiig"
-__date__ ="$Jan 16, 2014 2:43:42 PM$"
-
-if __name__ == "__main__":
-    print "Hello World"
