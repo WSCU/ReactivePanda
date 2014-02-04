dc98afae598fbf6e38493d16b56cdedd51c57031
WSCU <lucas.vandenbergh@western.edu>
2014-02-04 15:15:14 -0700

    Removed more old code in Signal

--- src/Signal.py
+++ src/Signal.py
@@ -1,14 +1,3 @@
-class FsSignal:
-    def __init__ (self, x):
-        self.x = x
-        self.startTime = Engine.globalTime
-    def now(self):
-        pass
-    def state(self, time):
-        pass
-    def start(self):
-        pass
-
 class Lift0:
     def __init__(self, v):
         self.now = v
