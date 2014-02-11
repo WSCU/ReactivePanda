daf80e3a165d832e438cefd273b29609fe3928e3
westernState <bendix.cunnyngham@western.edu>
2014-02-11 11:58:23 -0700

    Fixed testing in Engine

--- src/Engine.py
+++ src/Engine.py
@@ -24,31 +24,50 @@ def maybeLift(x):
     if t is type(1.0):
         return Lift0(x)
     return x
+    
+    
 i1 = integral(1)
-#i2 = integral(i1)
-engine(i1)
-#engine(i2)
+i2 = integral(i1)
+#engine(i1)
+engine(i2)
 
 
 # Here's our "unit".
 
-def IntegralTest():
+def IntegralTest1():
     count = 0 
     a = [0,1,2,3,4,5]
     s = integral(1)
     rs = s.start()
     for t in a:
-        if(a!=rs.now){return false}
-    return true
-    
-        
-        
+        if t!=rs.now:
+            return False
+        rs.refresh(1)
+    return True
+
+def IntegralTest2():
+    count = 0 
+    a = [0,1,3,6,10]
+    s1 = integral(1)
+    s2 = integral(s1)
+    rs = s2.start()
+    for t in a:
+        if t!=rs.now:
+            return False
+        rs.refresh(1)
+    return True
+
+
+
 class TestEngine(unittest.TestCase):
 
-    def test_Integral(self):
-        self.failUnless(IntegralTest)
+    def test_Integral1(self):
+        self.failUnless(IntegralTest1())
+        
+    def test_Integral2(self):
+        self.failUnless(IntegralTest2())
         
 
 engineTestSuite = unittest.TestSuite()
 engineTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestEngine)
-    #statNumerTestSuite.addTest(TestStaticNumerics('test_P2AddZero'))
+#unittest.TextTestRunner(verbosity=2).run(engineTestSuite)
--- src/Test.py
+++ src/Test.py
@@ -6,7 +6,7 @@ suite1 = engineTestSuite
 class AllTests(unittest.TestCase):
     
     def runTest(self):
-        suite1 
+        unittest.TextTestRunner(verbosity=2).run(suite1) 
 
 if __name__ == '__main__':
     unittest.main()
