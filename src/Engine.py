ccc1534e7649af6828d86b2399f15a83986e57a8
westernState <bendix.cunnyngham@western.edu>
2014-02-10 22:42:36 -0700

    Added testing harness and test for integral

--- src/Engine.py
+++ src/Engine.py
@@ -1,6 +1,8 @@
+import unittest
 import sched, time
 from StateMachine import *
 from Signal import *
+
 def integral(x):
     def integralFN(i, s, dt):
         c = s + i * dt
@@ -26,3 +28,27 @@ i1 = integral(1)
 #i2 = integral(i1)
 engine(i1)
 #engine(i2)
+
+
+# Here's our "unit".
+
+def IntegralTest():
+    count = 0 
+    a = [0,1,2,3,4,5]
+    s = integral(1)
+    rs = s.start()
+    for t in a:
+        if(a!=rs.now){return false}
+    return true
+    
+        
+        
+class TestEngine(unittest.TestCase):
+
+    def test_Integral(self):
+        self.failUnless(IntegralTest)
+        
+
+engineTestSuite = unittest.TestSuite()
+engineTestSuite = unittest.TestLoader().loadTestsFromTestCase(TestEngine)
+    #statNumerTestSuite.addTest(TestStaticNumerics('test_P2AddZero'))
