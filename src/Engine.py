ce39085f321c2961459b14a8adf23022a7a1ba6c
westernState <bendix.cunnyngham@western.edu>
2014-02-18 12:15:13 -0700

    Added lifting to the engine

--- src/Engine.py
+++ src/Engine.py
@@ -32,6 +32,44 @@ i2 = integral(i1)
 engine(i2)
 
 
+
+
+class Lift:
+    def __init__(self, f, args):
+        self.f = f
+        self.args=args
+    def now(self):
+    	ea = map (lambda a: a.now() , self.args)
+    	return self.f(*ea)
+    
+    
+
+    
+    
+    
+class Factory:
+	def __init__(self,f, args):
+		self.type = "factory"
+		self.f=f 
+		self.args = args
+	def __add__(self,x,y):
+		y=maybeLift(y)
+		return Factory(lambda x,y:x+y, [x,y])
+	def start(self):
+		return Lift(f,map(lambda x: x.start(), args))
+	
+	
+
+def lift(f):
+	def fn(*args):
+		return Factory(f,args)
+	return fn	
+	
+
+
+
+
+####################  Testing ################################
 # Here's our "unit".
 
 def IntegralTest1():
