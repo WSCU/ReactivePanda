6f6f1b0869d7cfcb9cde8c83dfdaa7cf6ecab5f8
westernState <bendix.cunnyngham@western.edu>
2014-03-13 14:41:44 -0600

    working in class on 3/13/14

--- src/Proxy.py
+++ src/Proxy.py
@@ -1,24 +1,34 @@
 import Globals
 
 class Proxy:
-	def __init__(self,* named, ** unnamed):
-		self.alive = True;
-		self.signals = {}
-		self.reactions = {}
+	def __init__(self,name):
+		self._alive = True;
+		self._signals = {}
+		self._1Reactions = [] # one time reactions
+		self._gReactions = [] # global reactions
+		self._updateDict = {}
+		Globals.world.add(self)
+		
 	def __setattr__(self,name, value):
 		#if hasattr(self,name):
+		#if name starts with ._ hand it over to the __dict__
+		#if name starts wiht .somethign then 
 		self.updateDict[name] = value
+		
+		#catch multiple updates on one name in a single heart beat
 		
 	def react(self):
 		if alive:
 			pass
 		else:
 			pass
+
 	def update(self):
 		if alive:
-			for k,v in signals:
+			for k,v in _signals:
 				v.now()
-			for k,v in self.updateDict:
+			for k,v in self._updateDict:
 				self.__dict__[k] = v
+			self._updateDict = {}
 		else:
 			pass
