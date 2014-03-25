0c63c2f24ff6f8b9131fcaa0b0562f4daf7f7e79
westernState <bendix.cunnyngham@western.edu>
2014-03-25 15:21:29 -0600

    Update Proxy.py

--- src/Proxy.py
+++ src/Proxy.py
@@ -1,13 +1,13 @@
 import Globals
 
 class Proxy:
-	def __init__(self,name):
+	def __init__(self,name, updater):
 		self._alive = True; # so the object can be a ghost 
 		self._signals = {} #users reactive signals
 		self._1Reactions = [] # one time reactions
 		self._gReactions = [] # global reactions
 		self._updateSignals = {} #synchronization barrier
-		
+		self._updater = updater # function pushes attributes to panda
 		Globals.world.add(self)
 		
 	def __setattr__(self,name, value):
@@ -21,13 +21,24 @@ class Proxy:
 			return self.__dict__[name]# check for undifined attributes
 		else:
 			return None# This is where we need to creat observer signals
+		#Observer(f) where f is a function that knows which object and which
+		#atribute when called it goes in and samples the value
+		
+	def initialize(self):
+		for k,v in _updateSignals:
+			self._signals[k] = v.start()
+		
+	def updater(self):
+		self._updater(self)
 		
 	def react(self,when,what):#when the reaction should happen and what it should do
 		if alive:
-			self._gReactions.append((what,when))# add to the list
+			maybeLift(when)
+			self._gReactions.append((when.start(),what))# add to the list
 	def react1(self,when,what):#when the reaction should happen and what it should do
 		if alive:
-			self._1Reactions.append((what,when))# add to the list
+			maybeLift(when)
+			self._1Reactions.append((when.start(),what))# add to the list
 		
 	def update(self):
 		if alive:
@@ -35,9 +46,15 @@ class Proxy:
 			for k,v in _signals:
 				v.now()for k,v in _signals:
 				v.now()
-				
+			thunks = []	
 		        for a in 1Reactions:
-		        	#if it happens remove it from the list
+		        	temp = a[0].now()
+		        	if temp != None:#if it happens remove it from the list
+		        		#add thunks
+		        		thunks.add(lambda : a[1](self, temp))
+		        
+		        if(len(thunks) >= 2):
+		        	print("Multiple 1 time reactions in a heartbeat")
 			for a in gReactions:
 				#leave them in
 			for k,v in self._updateSignals:
