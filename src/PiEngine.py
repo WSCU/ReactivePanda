2d9c56d049310a68396b725c7069e045813deae2
Graham <pi@raspberrypi.(none)>
2014-03-30 14:45:42 -0600

    YAYQ

--- src/PiEngine.py
+++ src/PiEngine.py
@@ -18,15 +18,15 @@ def heartBeat(ct, events):
     Globals.events = events
     Globals.thunks = []
     for worldObject in Globals.worldObjects:
-        print("Updating object: " + str(worldObject)
-        Globals.thunks.append(worldObject.update())
+        print("Updating object: " + str(worldObject))
+        Globals.thunks.extend(worldObject.update())
     for f in Globals.thunks:
         f()
     for obj in Globals.newModels:
-        print("Adding object: " + str(obj)
+        print("Adding object: " + str(obj))
         worldObjects[str(obj)] = obj
     for obj in Globals.worldObjects:
-        print("Initializing object: " + str(obj)
+        print("Initializing object: " + str(obj))
         obj.initialize()
 #will need to check the proxy module to find the right name for this initialize method
 #make an initialize method that clears out all the variables and resets the clock
