a0d2be4143f3027829a7cff286064460b1715eca
Jiig <luktut@gmail.com>
2014-04-22 14:08:57 -0600

    Fixed some bugs in the test functions

--- src/OfflineEngine.py
+++ src/OfflineEngine.py
@@ -109,10 +109,10 @@ if __name__ == "__main__":
 
 def mIntegrals():
     p = printer ("integal", i = integral(1))
-    q = ("integral 2", i = integral(p, 1))
+    q = printer ("integral 2", i = integral(p.i))
     engine(50, verbose = True)
     
 def holdTest():
     p = printer("Hold", i = hold(2))
-    mouseLeft([20, 25, 30,, 35, 40])
+    mouseLeft([20, 25, 30, 35, 40])
     engine(50, verbose = True)
