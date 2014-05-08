4666f3e764944c6b073e442a2bac33060742c694
isaacgibbs <dirtbiker201@bresnan.net>
2014-05-08 13:22:01 -0600

    Update Interp.py

--- src/Interp.py
+++ src/Interp.py
@@ -32,7 +32,7 @@ class Interp:
     """
     def __init__(self):
         self.interpolant = None
-        self._type = interpableType;
+        self._type = "interpableType";
     def __add__(self, x):
     	"""
     	Infix (+) operator
