31627acefd708263859035e727698728d4efd2e6
Graham <graham.montgomery@western.edu>
2014-06-20 21:22:56 -0600

    ParticleEffects testing file. all there

---
+++ tests/ParticleEffects.py
@@ -0,0 +1,21 @@
+from Panda import *
+
+world.color = black
+
+#fireish()
+fire()
+#fire1()
+#explosions()
+#fireWorks()
+#fireWork()
+#intervalRings()
+#fountain()
+#rain()
+#shakenSparkles()
+#warpSpeed()
+#warpFace()
+#heavySnow()
+#lightSnow()
+#smokeTail()
+
+start()
