94f132d09147e31050dd9584bdee7c394b1bb1f1
Graham <graham.montgomery@western.edu>
2014-06-21 23:00:08 -0600

    cdvsd

--- tests/CondecendingDragon.py
+++ tests/CondecendingDragon.py
@@ -6,6 +6,6 @@ dragon(position = p3(-3,5,-2), hpr=hpr(-5,0,0), size=4)
 bee(position = p3(1.1*sin(integral(time)-pi) , -1.1*cos(integral(time)-pi), -0.88), hpr = hpr(integral(time),0,0), color= cyan, size = .5)
 panda(position = p3(0,0,-1), hpr = hpr(integral(time),10,0), color=pink)
 fireish(position = p3(0,0,-1.5), size = .15)
-launchCamera("medevil")
+#launchCamera("medevil")
 
 start()
