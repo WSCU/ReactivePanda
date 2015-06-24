b4250bb4a874c884539cd039f47efece95cf995f
Graham <graham.montgomery@western.edu>
2015-06-24 14:30:47 -0600

    YAY RALPH!

--- tests/CondecendingDragon.py
+++ tests/CondecendingDragon.py
@@ -5,8 +5,8 @@ from ReactivePanda.Panda import *
 grassScene()
 b = ralph(position = p3(0,0,sin(time)/3-1))
 dragon(position = p3(-3,5,-2), hpr=hpr(-5,0,0), size=4)
-bee(position = p3(1.1*sin(integral(time, 0)-pi) , -1.1*cos(integral(time, 0)-pi), -0.88), hpr = hpr(integral(time, 0),0,0), color= cyan, size = .5)
-panda(position = p3(0,0,-1), hpr = hpr(integral(time, 0),10,0), color=pink)
+bee(position = p3(1.1*sin(integral(2, 0)-pi) , -1.1*cos(integral(2, 0)-pi), -0.88), hpr = hpr(integral(2, 0),0,0), color= cyan, size = .5)
+panda(position = p3(0,0,-1), hpr = hpr(integral(2, 0),10,0), color=pink)
 fireish(position = p3(0,0,-1.5), size = .15)
 #launchCamera("medevil")
 
