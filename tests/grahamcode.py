ce7c730c2bae0933ec30f07e88aa010805c993b0
Graham <graham.montgomery@western.edu>
2015-06-25 14:53:24 -0600

    xdfgsdfg

--- tests/grahamcode.py
+++ tests/grahamcode.py
@@ -1,12 +1,19 @@
 from ReactivePanda.Panda import *
 
-world.color = red
+world.color = green
 b = spiderman(position = p3(0,30,-1))
+pose(b,"DOIT!",frame=0)
 s = sound("DOIT.wav")
 
+def resetPose():
+    pose(b,"DOIT!",frame=0)
+
 def timeToStart(m,v):
-    b.play("DOIT!")
+    playAnim(b,"DOIT!")
     atTime(now(time)+3.5, s.play)
+    atTime(now(time)+6,resetPose)
+
+
 
 react(lbp(),timeToStart)
 
