0e0a751c1d56fcb1b32a41ab754ec6196cecb0a1
Miller Ridgeway <millerr2017@gmail.com>
2016-02-04 21:56:15 -0700

    Did a little bit of formatting and changed a few variables. Attempted to get the new addSignal notation working from the previous commit, but I am still encountering object call issues.

--- PandaWorld.py
+++ PandaWorld.py
@@ -1,27 +1,27 @@
-from pythonfrp import Proxy
 from pythonfrp import Globals
 from pythonfrp.Types import *
 from pythonfrp import Numerics
-from . Color import gray
+from pythonfrp import World
+from .Color import gray
+
 
 def updateColor(self):
     c = self._get("color")
-    base.setBackgroundColor(c.r, c.g, c.b) # What is base?
+    base.setBackgroundColor(c.r, c.g, c.b)  # What is base?
 
 
-#class World(Proxy.Proxy):
+# class World(Proxy.Proxy):
 #    def __init__(self):
 #        Proxy.Proxy.__init__(self, "world", updateWorld, {"color": colorType, "gravity": p3Type})
 #        self.color = gray # Set this in panda specific area
 #        self.gravity = Numerics.p3(0,0,-1)
-        
-#world = World()
-pWorld = World.world
 
-pWorld.addSignal("color",gray,colorType,updateColor)
-pWorld.addSignal("gravity",Numerics.p3(0,0,-1),p3Type,lambda: None)
+# world = World()
 
-def resetWorld(continueFn = lambda: None):
-    Globals.resetFlag=continueFn
+World.addSignal("color", gray, colorType, updateColor)
+World.world.addSignal("gravity", Numerics.p3(0, 0, -1), p3Type, lambda: None)
+
+
+def resetWorld(continueFn=lambda: None):
+    Globals.resetFlag = continueFn
     # Should make all DirectGUI stuff invisible
-
