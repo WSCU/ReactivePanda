00fdf46fd3d526ca00c06c1d8d115534b423e7e2
Miller Ridgeway <millerr2017@gmail.com>
2016-02-06 19:46:49 -0700

    Fixed the issue with the addSignal function
    Turns out that it wasn't an issue with the function, but the naming of the World class, module, and instance. The function addSignal belongs the the World module, as does the instance of World, pointed at by a lowercase w world. It gets a bit confusing and perhaps should be changed. Nonetheless, it works now.

--- PandaWorld.py
+++ PandaWorld.py
@@ -10,18 +10,16 @@ def updateColor(self):
     base.setBackgroundColor(c.r, c.g, c.b)  # What is base?
 
 
-# class World(Proxy.Proxy):
-#    def __init__(self):
-#        Proxy.Proxy.__init__(self, "world", updateWorld, {"color": colorType, "gravity": p3Type})
-#        self.color = gray # Set this in panda specific area
-#        self.gravity = Numerics.p3(0,0,-1)
-
-# world = World()
+world = World.world
+# This is confusing, and maybe needs to be changed: World.world is
+# an instance of the World class inside the World module
+# World.addSignal is inside the World module, bvt not the World class. world, defined here, is a pointer for the rest
+# of the engine. Should we re-name this? There are a lot of worlds, and it took me a while to figure them out.
 
 World.addSignal("color", gray, colorType, updateColor)
-World.world.addSignal("gravity", Numerics.p3(0, 0, -1), p3Type, lambda: None)
+World.addSignal("gravity", Numerics.p3(0, 0, -1), p3Type, lambda x: None)
 
 
-def resetWorld(continueFn=lambda: None):
-    Globals.resetFlag = continueFn
-    # Should make all DirectGUI stuff invisible
+def resetWorld():
+    World.resetWorld()
+    # Should we export this through engine somehow?
