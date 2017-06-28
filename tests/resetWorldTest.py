b484b27809bcff209fcd58d4baf3da1960afc60f
Miller <millerr2017@gmail.com>
2017-06-28 14:03:05 -0600

    Changed reset world test

--- tests/resetWorldTest.py
+++ tests/resetWorldTest.py
@@ -2,14 +2,29 @@ from PandaSRC.Panda import *
 # Background color didn't use to change
 world.color = blue
 jeep()
+
 def newWorld():
     print("Setting to red")
     world.color = red
-    panda()
+    panda1 = panda(position=p3(-3,0,0))
+    panda2 = panda()
+    hit(panda1, panda2, printText)
+    print("Time: " + str(now(time)))
+    react(clock(2, start=now(time)), printText)
+#    atTime(5, text)
+    print("After react call")
+    
 def doReset():
     print("Doing reset")
     resetWorld(newWorld)
     print("Done with reset")
-atTime(1, doReset)
+
+def printText(m, v):
+    text("Hello!")
+    print "adsa"
+    
+atTime(2, doReset)
+
+##### We need to double check why the function is being called with only 1 param
 
 start()
